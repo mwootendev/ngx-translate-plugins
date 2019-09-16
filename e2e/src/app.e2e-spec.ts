2850ef089cc25669179522d4167e6fbfc5c306ee
7jpsan <7jpsan@gmail.com>
2019-09-16 12:22:52 +0100

    Update module to support angular 8

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ngx-translate-testing!');
+    expect(page.getParagraphText()).toEqual('Welcome to The Translate Plugins Demo Application.');
   });
 });
