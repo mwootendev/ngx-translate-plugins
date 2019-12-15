0601cea7295845ecaec234f57a38ef717ea613fe
Michael Wooten <mwooten.dev@gmail.com>
2019-12-15 11:48:22 -0500

    WIP - Observable translations not working.

--- projects/missing-translations/src/public_api.ts
+++ projects/missing-translations/src/public_api.ts
@@ -1,7 +1,11 @@
 /*
- * Public API Surface of l10n
+ * Public API Surface of ngx-translate-missing-translations
  */
 
-export * from './lib/localized-fallback-translate-loader.service';
-export * from './lib/localized-missing-translation-handler.service';
-export * from './lib/l10n.module';
+export * from './lib/alternate-language-missing-translation-handler.service';
+export * from './lib/browser-culture-language-missing-translation-handler.service';
+export * from './lib/default-language-missing-translation-handler.service';
+export * from './lib/failover-missing-translation-handler.service';
+export * from './lib/language-missing-translation-handler.service';
+
+export * from './lib/missing-translations.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-import { TranslateLoader, TranslateModule, TranslateCompiler } from '@ngx-translate/core';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateCompiler,
+  MissingTranslationHandler,
+  FakeMissingTranslationHandler
+} from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
 
+import { FailoverMissingTranslationHandler, DefaultLanguagedMissingTranslationHandler, LanguageMissingTranslationHandler } from 'ngx-translate-missing-translations';
+
 import { AppComponent } from './app.component';
+
+import { LanguageSwitcherComponent } from './language-switcher.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LanguageSwitcherComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -22,14 +32,21 @@ export function HttpLoaderFactory(http: HttpClient) {
         provide: TranslateCompiler,
         useClass: TranslateMessageFormatCompiler
       },
+      useDefaultLang: false,
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
+      },
+      missingTranslationHandler: {
+        provide: MissingTranslationHandler,
+        useValue: new FailoverMissingTranslationHandler([
+          new LanguageMissingTranslationHandler('es'),
+          new DefaultLanguagedMissingTranslationHandler()
+        ])
       }
     })
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
