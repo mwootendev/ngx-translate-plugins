d7e78b1ca7523f3e341a782d8b146c978cd6b281
Michael Wooten <mwooten.dev@gmail.com>
2021-09-22 23:16:45 -0400

    37 - Fixes ngx-translate-testing Test Cases
    Resolves an issue where the test cases for ngx-translate-testing would
    not run successfully due to a change in the path for the "reflect"
    library within the core-js project.

--- projects/testing/src/test.ts
+++ projects/testing/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
@@ -12,10 +12,7 @@ import {
 declare const require: any;
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
